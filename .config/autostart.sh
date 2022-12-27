d5759e93d1fe77d0091833078a1219a1f3166ce2
andrius <andrius.solopovas@gmail.com>
2022-12-27 17:06:39 +0000

    save

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -4,14 +4,12 @@ picom --no-fading-openclose &
 .config/polybar/launch.sh &
 xrandr --output DP-2 --primary --mode 1920x1080 --rate 240 --output DP-4 --rate 240 --right-of DP-2
 xsetroot -cursor_name left_ptr &
-autorandr --change --force &
 flameshot &
 nm-applet &
 clipit &
 blueman-applet &
 setbg &
 skypeforlinux &
-signal-desktop &
 telegram-desktop &
 cryptomator &
 insync start
