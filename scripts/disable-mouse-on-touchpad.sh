1b33c7dce5c6fa190c5fcab6b3cf09fa6c00efe1
andrius <andrius.solopovas@gmail.com>
2023-01-16 19:31:59 +0000

    save

--- scripts/disable-mouse-on-touchpad.sh
+++ scripts/disable-mouse-on-touchpad.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-touchpad_rule="/etc/udev/rules.d/99-touchpad.rules"
+#create the udev rule file
+touchpad_rule="/etc/udev/rules.d/01-touchpad.rules"
+xauthority=/home/$(whoami)/.Xauthority
+sudo touch $touchpad_rule
+sudo chmod 666 $touchpad_rule
 
-echo 'ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="*Mouse*", ATTR{[14]}="0"' | sudo tee $touchpad_rule
-echo 'ACTION=="remove", SUBSYSTEM=="input", ATTRS{name}=="*Mouse*", ATTR{[14]}="1"' | sudo tee -a $touchpad_rule
+template='SUBSYSTEM=="input", KERNEL=="mouse[0-9]*", ACTION=="add", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/username/.Xauthority", RUN+="/usr/bin/synclient TouchpadOff=1"'
+
+echo $template | sed -e "s/username/$(whoami)/" | sudo tee $touchpad_rule
+echo $template | sed -e "s/username/$(whoami)/" | sed -e 's/add/remove/' -e 's/TouchpadOff=1/TouchpadOff=0/' | sudo tee -a $touchpad_rule
+
+# reload the udev rules
 sudo udevadm control --reload-rules
 
